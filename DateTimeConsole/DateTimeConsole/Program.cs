56321616f4d6f734176a458de2f04f6d48f02e21
Britta <Britta@Britta-MacBook-Air.local>
2019-11-04 20:55:48 +0200

    DateTimeApp

---
+++ DateTimeConsole/DateTimeConsole/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace DateTimeConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*var dateToday = new DateTime(1991,5,22);
+            Console.WriteLine(dateToday);
+            var dayOfTheWeek = dateToday.DayOfWeek;
+            Console.WriteLine(dayOfTheWeek);*/
+
+
+            /*Console.WriteLine("Please enter your birthdate(YYYY/MM/DD): ");
+            string userData = Console.ReadLine();
+
+            string[] personalData = userData.Split('/');
+            int year = int.Parse(personalData[0]);
+            int month = int.Parse(personalData[1]);
+            int day = int.Parse(personalData[2]);
+
+            var userBirthday = new DateTime(year, month, day);
+            var birtdayDayOfTheWeek = userBirthday.DayOfWeek;
+
+            Console.WriteLine($"You are born on {birtdayDayOfTheWeek}.");*/
+
+            var dateObject = new DateTime(2019,11,1);
+            var currentDate = DateTime.Now;
+            Console.WriteLine(currentDate);
+
+            var currentDayOfTheWeek = currentDate.DayOfWeek;
+            Console.WriteLine(currentDayOfTheWeek);
+
+            var ticks = dateObject.Ticks;//second since Jan 1, 1970
+            Console.WriteLine($"Ticks {ticks}");
+            Console.WriteLine($"Print current date: {currentDate}");
+            Console.WriteLine($"Print long date string: {currentDate.ToLongDateString()}");
+            Console.WriteLine($"Print short date string: {currentDate.ToShortDateString()}");
+            Console.WriteLine($"Print long time string: {currentDate.ToLongTimeString()}");
+            Console.WriteLine($"Print short time string: {currentDate.ToShortTimeString()}");
+            Console.WriteLine($"Date format: {currentDate.ToString("dd-MM-yyyy hh:mm")}");
+
+
+
+
+        }
+    }
+}
---
+++ DateTimeConsole/MyList/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() { 1, 2, 3, 4, 5 };
+            numbers.Add(1);//lisab 1 muutuja massivi juurde
+            numbers.AddRange(new int[3] { 6, 7, 8 });
+
+
+            foreach (int element in numbers)
+            {
+                Console.WriteLine(element);
+            }
+
+
+            int index = numbers.IndexOf(1);
+            Console.WriteLine($"Value of IndexOf: {index}");
+            int lastIndex = numbers.LastIndexOf(1);
+            Console.WriteLine($"Value of LastIndexOf: {lastIndex}");
+            //numbers.RemoveAt(lastIndex);
+
+            foreach (int element in numbers)
+            {
+                Console.WriteLine(element);
+            }
+
+            int sizeOfList = numbers.Count;
+            Console.WriteLine($"Your list is {sizeOfList} long.");
+
+            for(int i = 0; i < numbers.Count; i++)
+            {
+                if(numbers[i] == 1)
+                {
+                    numbers.Remove(numbers[i]);
+                }
+            }
+            //empty the list
+            numbers.Clear();
+
+            foreach (int element in numbers)
+            {
+                Console.WriteLine($"List after Clear() {element}");
+            }
+
+
+        }
+    }
+}
